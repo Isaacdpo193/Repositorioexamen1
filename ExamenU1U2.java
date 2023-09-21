819f0e380b746952a66b89c632220d567f697ea6
Isaacdpo193 <143726091+Isaacdpo193@users.noreply.github.com>
2023-09-21 08:25:04 -0500

    Add files via upload

---
+++ ExamenU1U2.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenu1u2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author labcca
+ */
+public class ExamenU1U2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        double videojuego1,videojuego2,videojuego3;
+        double precio,descuento,resultado,porcentaje = 100;
+        
+      
+        System.out.println("Ingresa el precio del primer videojuego; ");
+        videojuego1 = teclado.nextDouble();
+        
+        resultado = descuento;
+        descuento = resultado/porcentaje;
+        
+        System.out.println("El precio final del videojuego1 es;");
+        videojuego1 = teclado.nextDouble();
+        
+        System.out.println("Ingresa el precio del segundo videojuego; ");
+        videojuego2 = teclado.nextDouble();
+        
+        resultado = descuento;
+        descuento = resultado/porcentaje;
+        
+        System.out.println("El precio final del videojuego1 es;");
+        videojuego2 = teclado.nextDouble();
+        
+        System.out.println("Ingresa el precio del tercer videojuego; ");
+        videojuego3 = teclado.nextDouble();
+        
+        resultado = descuento;
+        descuento = resultado/porcentaje;
+        
+        System.out.println("El precio final del videojuego1 es;");
+        videojuego2 = teclado.nextDouble();
+    }
+    
+}
